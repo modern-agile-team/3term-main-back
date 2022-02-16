a395c6b9182277f9597d3a7652907e94707deb91
Yonghoon <yonghoonj98@gmail.com>
2022-02-16 10:45:45 +0900

    jungyonghoon(Refactor/maria): 코드 수정

--- app/src/config/maria.js
+++ app/src/config/maria.js
@@ -1,15 +1,13 @@
 "use strict";
-const maria = require("mariadb");
+const db = require("mariadb");
 
 const { DB_HOST, DB_USER, DB_PSWORD, DB_DATABASE } = process.env;
 
-const db = maria.createPool({
+const maria = db.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PSWORD,
   database: DB_DATABASE,
 });
 
-// db.connect();
-
-module.exports = db;
+module.exports = maria;
