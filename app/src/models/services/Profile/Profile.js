0a6181cf5a106c6838f18d8cffec101e5b3be932
Yonghoon <yonghoonj98@gmail.com>
2022-02-18 16:19:11 +0900

    Add(jungyonghoon/profile) 프로필 조회 초기 코드 작성 #8

--- app/src/models/services/Profile/Profile.js
+++ app/src/models/services/Profile/Profile.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const ProfileStorage = require("./ProfileStorage");
+
+class Profile {
+  constructor(req) {
+    this.params = req.params;
+    this.body = req.body;
+    this.query = req.query;
+  }
+
+  async oneLookUp() {
+    try {
+      return await ProfileStorage.findProfile();
+    } catch (err) {
+      throw { msg: err.msg };
+    }
+  }
+}
+
+module.exports = Profile;
--- app/src/models/services/Profile/ProfileStorage.js
+++ app/src/models/services/Profile/ProfileStorage.js
@@ -1,1 +1,21 @@
 "use strict";
+const maria = require("../../../config/maria");
+
+class ProfileStorage {
+  static async findProfile() {
+    let conn;
+
+    try {
+      conn = await maria.getConnection();
+      const query = `SELECT * FROM profiles`;
+
+      return await conn.query(query);
+    } catch (err) {
+      throw {
+        msg: "오류",
+      };
+    }
+  }
+}
+
+module.exports = ProfileStorage;
