758f9bb0c474dcdbe9263b61524c4611e1b77c71
Yonghoon <yonghoonj98@gmail.com>
2022-03-01 16:08:57 +0900

    루트 경로 DB 연동

--- mohae/src/app.module.ts
+++ mohae/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const { DB_HOST, DB_DATABASE, DB_USER, DB_PSWORD, DB_PORT } = process.env;
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: DB_HOST,
+      port: Number(DB_PORT),
+      username: DB_USER,
+      password: DB_PSWORD,
+      database: DB_DATABASE,
+      entities: [],
+      synchronize: true, // 개발 모드일 때만 사용
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
