71c72eaecc8d6e572e67b7d2ee74f754be484744
Yonghoon <yonghoonj98@gmail.com>
2022-03-08 13:45:09 +0900

    Add(jungyonghoon/category): 카테고리 폴더 구조 생성 #19

--- mohae/src/app.module.ts
+++ mohae/src/app.module.ts
@@ -6,9 +6,10 @@ import { typeORMConfig } from './configs/typeorm.config';
 <<<<<<< HEAD
 import { ReportsModule } from './reports/reports.module';
 import { FaqsModule } from './faqs/faqs.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), ReportsModule, FaqsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), ReportsModule, FaqsModule, CategoriesModule],
 =======
 
 @Module({
