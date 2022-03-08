3bc2a127f45d0e9072807ad013c698ec36035f30
Yonghoon <yonghoonj98@gmail.com>
2022-03-08 12:38:16 +0900

    Add(jungyonghoon/faqs): FAQ 로그 적용(Logger) #15

--- mohae/src/faqs/faqs.controller.ts
+++ mohae/src/faqs/faqs.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { Faq } from './entity/faq.entity';
 import { FaqsService } from './faqs.service';
 
 @Controller('faqs')
 export class FaqsController {
+  private logger = new Logger('FaqsController');
   constructor(private faqsService: FaqsService) {}
 
   @Get()
   findAllFaqs(): Promise<Faq[]> {
+    this.logger.verbose(`The user accessed the FAQ.`);
     return this.faqsService.findAllFaqs();
   }
 }
