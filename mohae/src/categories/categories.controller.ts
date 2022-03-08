6f7b9f835b9bf8ff3f47f5b7f042e58450a471d7
Yonghoon <yonghoonj98@gmail.com>
2022-03-08 14:41:58 +0900

    Add(jungyonghoon/category): 카테고리 전체 조회 기능 로그 적용 #19

--- mohae/src/categories/categories.controller.ts
+++ mohae/src/categories/categories.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { Category } from './entity/category.entity';
 
 @Controller('categories')
 export class CategoriesController {
+  private logger = new Logger('CategoriesController');
   constructor(private categoryService: CategoriesService) {}
 
   @Get()
   findAllCategories(): Promise<Category[]> {
+    this.logger.verbose(`카테고리 전체 조회 시도`);
     return this.categoryService.findAllCategories();
   }
 }
