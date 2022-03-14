4263753909bea8de706a24e0800beb1bf0ed6b5a
subro <suhyung,lee9810@gmail.com>
2022-03-14 20:55:27 +0900

    Add(leesuhyung/auth): 로그인시 JWT 토큰 발급 #30

--- mohae/src/auth/auth.service.ts
+++ mohae/src/auth/auth.service.ts
@@ -4,23 +4,28 @@ import { CreateBoardDto } from 'src/boards/dto/board.dto';
 import { CreateUserDto, SignInDto } from './dto/auth-credential.dto';
 import { UserRepository } from './repository/user.repository';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
   async signUp(createUserDto: CreateUserDto): Promise<void> {
     return this.userRepository.createUser(createUserDto);
   }
 
-  async signIn(signInDto: SignInDto): Promise<string> {
+  async signIn(signInDto: SignInDto): Promise<{ accessToken: string }> {
     const { email, password } = signInDto;
     const user = await this.userRepository.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.salt))) {
-      return '로그인 성공';
+      const payload = { email };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
     } else {
       throw new UnauthorizedException('로그인 실패');
     }
