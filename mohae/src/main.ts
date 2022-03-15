30822645c9c85fcc7e0d192057a2368cf68d3ae9
Yonghoon <yonghoonj98@gmail.com>
2022-03-15 15:14:34 +0900

    Add(jungyonghoon/hotreload): Hot reload 수정

--- mohae/src/main.ts
+++ mohae/src/main.ts
@@ -10,22 +10,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
   const port = serverConfig.port;
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //     transform: true,
-  //   }),
-  // );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(port);
   Logger.log(`Start Run ${port}`);
 
-  // 핫 리로드를 위한 조건문
   if (module.hot) {
-    const connection = getConnection();
-    if (connection.isConnected) {
-      await connection.close();
-    }
     module.hot.accept();
     module.hot.dispose(() => {
       app.close();
