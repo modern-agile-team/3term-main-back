f8e8191f77ddeb86af3313c1e79f65c69e322c1e
Seung Bum <wer06099@naver.com>
2022-03-16 10:38:42 +0900

    Refactor(seungbum/areas): city, ward 테이블을 지운뒤 city 항목 areas 병합

--- mohae/src/wards/repository/ward.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Ward } from '../entity/wards.entity';
-
-@EntityRepository(Ward)
-export class WardsRepository extends Repository<Ward> {}
