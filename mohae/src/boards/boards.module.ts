908d18d832a7d64072661088a9174f95fc8b9262
Yonghoon <yonghoonj98@gmail.com>
2022-03-16 21:53:23 +0900

    Fix(jungyonghoon/board): 게시글 기능 에러 해결 #11

--- mohae/src/boards/boards.module.ts
+++ mohae/src/boards/boards.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { CategoriesService } from 'src/categories/categories.service';
+import { CategoryRepository } from 'src/categories/repository/category.repository';
 import { ReviewRepository } from 'src/reviews/repository/review.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { BoardRepository } from './repository/board.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository, ReviewRepository])],
+  imports: [
+    TypeOrmModule.forFeature([BoardRepository, ReviewRepository]),
+    TypeOrmModule.forFeature([CategoryRepository]),
+    CategoriesModule,
+  ],
   controllers: [BoardsController],
-  providers: [BoardsService],
+  providers: [BoardsService, CategoriesService],
 })
 export class BoardsModule {}
