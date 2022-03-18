63138486477e2d6836235cb37eb794e315a31741
Your Name <you@example.com>
2022-03-18 11:25:23 +0900

    Add(seungbum/board): note R 기능 작성 #11

---
+++ mohae/src/notes/notes.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Note } from './entity/note.entity';
+import { NoteRepository } from './repository/note.repository';
+
+@Injectable()
+export class NotesService {
+    constructor(
+        @InjectRepository(NoteRepository)
+        private noteRepository: NoteRepository
+    ) {}
+
+    async getAllNotes(): Promise<Note[]> {
+        return this.noteRepository.find()
+    }
+}
