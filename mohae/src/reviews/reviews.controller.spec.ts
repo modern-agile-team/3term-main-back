4b3f312660557107d3a110f1da91b66a5deab8ec
Yonghoon <yonghoonj98@gmail.com>
2022-03-21 12:26:40 +0900

    Add(jungyonghoon/test): Review 테스트 코드 틀 작성

--- mohae/src/reviews/reviews.controller.spec.ts
+++ mohae/src/reviews/reviews.controller.spec.ts
@@ -1,19 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewsController } from './reviews.controller';
-import { ReviewsService } from './reviews.service';
 
 describe('ReviewsController', () => {
   let reviewsController: ReviewsController;
-  let reviewsService: ReviewsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewsService],
       controllers: [ReviewsController],
     }).compile();
 
     reviewsController = module.get<ReviewsController>(ReviewsController);
-    reviewsService = module.get<ReviewsService>(ReviewsService);
   });
 
   it('should be defined', () => {
