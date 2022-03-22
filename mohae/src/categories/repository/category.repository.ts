39a1c33760d055868bf84def78a7d29e56da810a
Yonghoon <yonghoonj98@gmail.com>
2022-03-22 14:49:52 +0900

    Refactor(jungyonghoon/category): 카테고리 리팩토링 #19

--- mohae/src/categories/repository/category.repository.ts
+++ mohae/src/categories/repository/category.repository.ts
@@ -1,5 +1,19 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { Category } from '../entity/category.entity';
 
 @EntityRepository(Category)
-export class CategoryRepository extends Repository<Category> {}
+export class CategoryRepository extends Repository<Category> {
+  async findAllCategory(): Promise<Category[]> {
+    try {
+      const categories = await this.createQueryBuilder('categories')
+        .leftJoinAndSelect('categories.boards', 'boards')
+        .where('categories.boards = boards.no')
+        .getMany();
+      console.log(categories);
+      return categories;
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
+  }
+}
