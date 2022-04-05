19ef9dd9e7b66d4435eda3a0a1ddccc82a32639e
subro <suhyung,lee9810@gmail.com>
2022-04-05 13:57:32 +0900

    Add(suhyunglee/spec): 스팩 수정기능 DTO 설정 안했고, 기능만 구현 #99

--- mohae/src/specs/specs.controller.ts
+++ mohae/src/specs/specs.controller.ts
@@ -1,24 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { CreateSpecDto } from './dto/spec.dto';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateSpecDto, UpdateSpecDto } from './dto/spec.dto';
 import { SpecsService } from './specs.service';
 
 @Controller('specs')
 export class SpecsController {
   constructor(private specsService: SpecsService) {}
 
-  @Post('/regist')
-  async registSpec(@Body() createSpecDto: CreateSpecDto) {
-    try {
-      await this.specsService.registSpec(createSpecDto);
-
-      return Object.assign({
-        statusCode: 200,
-        msg: '성공적으로 스팩등록이 되었습니다.',
-      });
-    } catch (err) {
-      throw err;
-    }
-  }
   @Get('/user/:no')
   async getAllSpec(@Param('no') no: number) {
     try {
@@ -28,6 +15,34 @@ export class SpecsController {
         statusCode: 200,
         msg: '성공적으로 스펙을 불러왔습니다.',
         specs,
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  @Post('/regist')
+  async registSpec(@Body() createSpecDto: CreateSpecDto) {
+    try {
+      await this.specsService.registSpec(createSpecDto);
+
+      return Object.assign({
+        statusCode: 201,
+        msg: '성공적으로 스팩등록이 되었습니다.',
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  @Patch('/:no')
+  async updateSpec(@Param('no') specNo: number, @Body() updateSpec) {
+    try {
+      await this.specsService.updateSpec(specNo, updateSpec);
+
+      return Object.assign({
+        statusCode: 204,
+        msg: '성공적으로 스팩이 수정되었습니다.',
       });
     } catch (err) {
       throw err;
