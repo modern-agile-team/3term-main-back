0d29e8a703103f079bd46c9583f7da27303a3b82
Yonghoon <yonghoonj98@gmail.com>
2022-04-21 13:50:01 +0900

    Add(jungyonghoon/review): 리뷰 서비스 테스트 코드 작성(전체 조회, 선택 조회, 생성) #7

--- mohae/src/reviews/reviews.service.spec.ts
+++ mohae/src/reviews/reviews.service.spec.ts
@@ -1,49 +1,236 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserRepository } from 'src/auth/repository/user.repository';
 import { BoardRepository } from 'src/boards/repository/board.repository';
-import { Review } from './entity/review.entity';
+import { ErrorConfirm } from 'src/utils/error';
+import { Repository } from 'typeorm';
+import { CreateReviewDto } from './dto/review.dto';
 import { ReviewRepository } from './repository/review.repository';
 import { ReviewsService } from './reviews.service';
 
-const reviews: Review[] = [];
+const MockReviewRepository = () => ({
+  readAllReview: jest.fn(),
+  readUserReviews: jest.fn(),
+  createReview: jest.fn(),
+});
+
+const MockUserRepository = () => ({
+  findOne: jest.fn(),
+  save: jest.fn(),
+});
+
+const MockBoardRepository = () => ({
+  findOne: jest.fn(),
+  save: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('ReviewsService', () => {
   let reviewsService: ReviewsService;
-  let reviewRepository: ReviewRepository;
-  let boardRepository: BoardRepository;
+  let reviewRepository: MockRepository<ReviewRepository>;
+  let userRepository: MockRepository<UserRepository>;
+  let boardRepository: MockRepository<BoardRepository>;
+  let errorConfirm: ErrorConfirm;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewsService, ReviewRepository, BoardRepository],
+      providers: [
+        ReviewsService,
+        ErrorConfirm,
+        {
+          provide: getRepositoryToken(ReviewRepository),
+          useValue: MockReviewRepository(),
+        },
+        {
+          provide: getRepositoryToken(UserRepository),
+          useValue: MockUserRepository(),
+        },
+        {
+          provide: getRepositoryToken(BoardRepository),
+          useValue: MockBoardRepository(),
+        },
+      ],
     }).compile();
 
     reviewsService = module.get<ReviewsService>(ReviewsService);
-    reviewRepository = module.get<ReviewRepository>(ReviewRepository);
-    boardRepository = module.get<BoardRepository>(BoardRepository);
+    reviewRepository = module.get<MockRepository<ReviewRepository>>(
+      getRepositoryToken(ReviewRepository),
+    );
+    userRepository = module.get<MockRepository<UserRepository>>(
+      getRepositoryToken(UserRepository),
+    );
+    boardRepository = module.get<MockRepository<BoardRepository>>(
+      getRepositoryToken(BoardRepository),
+    );
+    errorConfirm = module.get<ErrorConfirm>(ErrorConfirm);
   });
 
   it('should be defined', () => {
     expect(reviewsService).toBeDefined();
   });
 
-  describe('findAllReview', () => {
+  describe('readReview', () => {
     it('모든 리뷰를 반환', async () => {
-      jest
-        .spyOn(reviewRepository, 'findAllReview')
-        .mockResolvedValue(Promise.resolve(reviews));
+      reviewRepository['readAllReview'].mockResolvedValue([
+        {
+          no: 1,
+          description: '테트 가아',
+          createdAt: '2022-04-13T14:18:50.911Z',
+          board: 15,
+          reviewer: 1,
+        },
+        {
+          no: 2,
+          description: '테트 가아',
+          createdAt: '2022-04-13T14:18:50.911Z',
+          board: 15,
+          reviewer: 2,
+        },
+      ]);
+      const returnValue = await reviewsService.readAllReview();
 
-      const result = await reviewsService.findAllReview();
+      expect(returnValue).toStrictEqual([
+        {
+          no: 1,
+          description: '테트 가아',
+          createdAt: '2022-04-13T14:18:50.911Z',
+          board: 15,
+          reviewer: 1,
+        },
+        {
+          no: 2,
+          description: '테트 가아',
+          createdAt: '2022-04-13T14:18:50.911Z',
+          board: 15,
+          reviewer: 2,
+        },
+      ]);
+    });
 
-      expect(result).toBeInstanceOf(Array);
+    it('하나의 리뷰 조회', async () => {
+      reviewRepository['readUserReviews'].mockResolvedValue([
+        {
+          no: 17,
+          description: '가나라바사ㅆㅆㅆ ㅆㅆㅅ',
+          rating: 5,
+          createdAt: '2022-04-13T14:39:42.063Z',
+          board: {
+            no: 52,
+            title: 'test2',
+          },
+          reviewer: {
+            no: 5,
+            photo_url: 'aedesdddfasdf',
+            nickname: 'aaaaa',
+          },
+        },
+        {
+          no: 23,
+          description: '가나라바사ㅆㅆㅆ ㅆㅆㅅ',
+          rating: 1,
+          createdAt: '2022-04-13T14:52:21.553Z',
+          board: {
+            no: 52,
+            title: 'test2',
+          },
+          reviewer: {
+            no: 1,
+            photo_url: '바뀐 사진 url',
+            nickname: '바뀐 닉네임!!',
+          },
+        },
+      ]);
+
+      const returnValue = await reviewsService.readUserReviews(1);
+
+      expect(returnValue).toStrictEqual({
+        rating: '3.0',
+        reviews: [
+          {
+            no: 17,
+            description: '가나라바사ㅆㅆㅆ ㅆㅆㅅ',
+            rating: 5,
+            createdAt: '2022-04-13T14:39:42.063Z',
+            board: {
+              no: 52,
+              title: 'test2',
+            },
+            reviewer: {
+              no: 5,
+              photo_url: 'aedesdddfasdf',
+              nickname: 'aaaaa',
+            },
+          },
+          {
+            no: 23,
+            description: '가나라바사ㅆㅆㅆ ㅆㅆㅅ',
+            rating: 1,
+            createdAt: '2022-04-13T14:52:21.553Z',
+            board: {
+              no: 52,
+              title: 'test2',
+            },
+            reviewer: {
+              no: 1,
+              photo_url: '바뀐 사진 url',
+              nickname: '바뀐 닉네임!!',
+            },
+          },
+        ],
+      });
     });
   });
 
-  describe('findOneReview', () => {
-    it.todo('리뷰 한 개 조회');
-    // it('리뷰 한 개 조회', async () => {
-    //   const reviewNo = 10;
-    //   const review = reviewsService.findOneReview(reviewNo);
-    //   console.log(review);
-    //   expect(review).toEqual(1);
-    // });
+  describe('createReview', () => {
+    it('리뷰 한 개 생성', async () => {
+      reviewRepository['createReview'].mockResolvedValue({
+        no: 1,
+        boardNo: [
+          {
+            no: 1,
+            title: '테스트 게시글',
+          },
+        ],
+        reviewerNo: [
+          {
+            no: 1,
+            name: '테스트 유저',
+          },
+        ],
+      });
+      boardRepository['findOne'].mockResolvedValue({
+        no: 1,
+        reviews: [],
+      });
+      userRepository['findOne'].mockResolvedValue({
+        no: 1,
+        reviews: [],
+      });
+
+      const createReviewDto: CreateReviewDto = {
+        boardNo: 1,
+        reviewerNo: 1,
+        description: '테스트 내용',
+        rating: 5,
+      };
+      const returnValue = await reviewsService.createReview(createReviewDto);
+
+      expect(returnValue).toStrictEqual({
+        no: 1,
+        boardNo: [
+          {
+            no: 1,
+            title: '테스트 게시글',
+          },
+        ],
+        reviewerNo: [
+          {
+            no: 1,
+            name: '테스트 유저',
+          },
+        ],
+      });
+    });
   });
 });
