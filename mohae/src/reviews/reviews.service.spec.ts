5788c8af8e9f7dc300a064376b90798d2ca436be
Yonghoon <yonghoonj98@gmail.com>
2022-04-21 14:58:46 +0900

    Add(jungyonghoon/review): 리뷰 서비스 생성할 때 예외처리 테스트 코드 작성 #7

--- mohae/src/reviews/reviews.service.spec.ts
+++ mohae/src/reviews/reviews.service.spec.ts
@@ -74,36 +74,68 @@ describe('ReviewsService', () => {
     it('모든 리뷰를 반환', async () => {
       reviewRepository['readAllReview'].mockResolvedValue([
         {
-          no: 1,
-          description: '테트 가아',
-          createdAt: '2022-04-13T14:18:50.911Z',
-          board: 15,
-          reviewer: 1,
+          no: 17,
+          description: '가나라바사ㅆㅆㅆ ㅆㅆㅅ',
+          rating: 5,
+          createdAt: '2022-04-13T14:39:42.063Z',
+          board: {
+            no: 52,
+            title: 'test2',
+          },
+          reviewer: {
+            no: 5,
+            photo_url: 'aedesdddfasdf',
+            nickname: 'aaaaa',
+          },
         },
         {
-          no: 2,
-          description: '테트 가아',
-          createdAt: '2022-04-13T14:18:50.911Z',
-          board: 15,
-          reviewer: 2,
+          no: 23,
+          description: '가나라바사ㅆㅆㅆ ㅆㅆㅅ',
+          rating: 1,
+          createdAt: '2022-04-13T14:52:21.553Z',
+          board: {
+            no: 52,
+            title: 'test2',
+          },
+          reviewer: {
+            no: 1,
+            photo_url: '바뀐 사진 url',
+            nickname: '바뀐 닉네임!!',
+          },
         },
       ]);
       const returnValue = await reviewsService.readAllReview();
 
       expect(returnValue).toStrictEqual([
         {
-          no: 1,
-          description: '테트 가아',
-          createdAt: '2022-04-13T14:18:50.911Z',
-          board: 15,
-          reviewer: 1,
+          no: 17,
+          description: '가나라바사ㅆㅆㅆ ㅆㅆㅅ',
+          rating: 5,
+          createdAt: '2022-04-13T14:39:42.063Z',
+          board: {
+            no: 52,
+            title: 'test2',
+          },
+          reviewer: {
+            no: 5,
+            photo_url: 'aedesdddfasdf',
+            nickname: 'aaaaa',
+          },
         },
         {
-          no: 2,
-          description: '테트 가아',
-          createdAt: '2022-04-13T14:18:50.911Z',
-          board: 15,
-          reviewer: 2,
+          no: 23,
+          description: '가나라바사ㅆㅆㅆ ㅆㅆㅅ',
+          rating: 1,
+          createdAt: '2022-04-13T14:52:21.553Z',
+          board: {
+            no: 52,
+            title: 'test2',
+          },
+          reviewer: {
+            no: 1,
+            photo_url: '바뀐 사진 url',
+            nickname: '바뀐 닉네임!!',
+          },
         },
       ]);
     });
@@ -184,21 +216,7 @@ describe('ReviewsService', () => {
 
   describe('createReview', () => {
     it('리뷰 한 개 생성', async () => {
-      reviewRepository['createReview'].mockResolvedValue({
-        no: 1,
-        boardNo: [
-          {
-            no: 1,
-            title: '테스트 게시글',
-          },
-        ],
-        reviewerNo: [
-          {
-            no: 1,
-            name: '테스트 유저',
-          },
-        ],
-      });
+      reviewRepository['createReview'].mockResolvedValue(1);
       boardRepository['findOne'].mockResolvedValue({
         no: 1,
         reviews: [],
@@ -218,6 +236,75 @@ describe('ReviewsService', () => {
 
       expect(returnValue).toStrictEqual({
         success: true,
+      });
+    });
+
+    describe('생성에러', () => {
+      it('리뷰 생성시 게시글이 없을 경우', async () => {
+        reviewRepository['createReview'].mockResolvedValue(1);
+        userRepository['findOne'].mockResolvedValue({
+          no: 1,
+          reviews: [],
+        });
+
+        const createReviewDto: CreateReviewDto = {
+          boardNo: 1,
+          reviewerNo: 1,
+          description: '테스트 내용',
+          rating: 5,
+        };
+
+        try {
+          await reviewsService.createReview(createReviewDto);
+        } catch (e) {
+          expect(e.message).toBe('리뷰를 작성하려는 게시글이 없습니다.');
+        }
+      });
+
+      it('리뷰 생성시 리뷰어 정보가 없을 경우', async () => {
+        reviewRepository['createReview'].mockResolvedValue(1);
+        boardRepository['findOne'].mockResolvedValue({
+          no: 1,
+          reviews: [],
+        });
+
+        const createReviewDto: CreateReviewDto = {
+          boardNo: 1,
+          reviewerNo: 1,
+          description: '테스트 내용',
+          rating: 5,
+        };
+
+        try {
+          await reviewsService.createReview(createReviewDto);
+        } catch (e) {
+          expect(e.message).toBe('리뷰 작성자를 찾을 수 없습니다.');
+        }
+      });
+
+      it('리뷰 생성이 되지 않았을 경우', async () => {
+        reviewRepository['createReview'].mockResolvedValue(0);
+        userRepository['findOne'].mockResolvedValue({
+          no: 1,
+          reviews: [],
+        });
+        boardRepository['findOne'].mockResolvedValue({
+          no: 1,
+          reviews: [],
+        });
+
+        const createReviewDto: CreateReviewDto = {
+          boardNo: 1,
+          reviewerNo: 1,
+          description: '테스트 내용',
+          rating: 5,
+        };
+
+        try {
+          await reviewsService.createReview(createReviewDto);
+        } catch (e) {
+          expect(e.message).toBe('알 수 없는 리뷰 작성 오류');
+        }
       });
     });
   });
