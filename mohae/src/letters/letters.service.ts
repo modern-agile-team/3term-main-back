78eb65e9895c393f2ad5fbf04a2b143e1a37532d
Yonghoon <yonghoonj98@gmail.com>
2022-04-28 16:31:37 +0900

    Refactor(jungyonghoon/letters): 쪽지함 리팩토링에 따라 쪽지 전송 기능 리팩토링 #6

--- mohae/src/letters/letters.service.ts
+++ mohae/src/letters/letters.service.ts
@@ -39,64 +39,96 @@ export class LettersService {
     description,
   }: SendLetterDto) {
     try {
-      const newMailboxNo = !mailboxNo
-        ? await this.mailboxRepository.createMailbox()
-        : mailboxNo;
-      if (!newMailboxNo) {
-        throw new InternalServerErrorException('쪽지 보내기 에러');
-      }
-      const mailboxRelation = await this.mailboxRepository.findOne(
-        newMailboxNo,
-        {
-          select: ['no'],
-          relations: ['letters', 'mailboxUsers'],
-        },
-      );
-      // console.log(mailboxRelation, newMailboxNo);
       const sender = await this.userRepository.findOne(senderNo, {
-        select: ['no'],
         relations: ['sendLetters', 'mailboxUsers'],
       });
-      this.errorConfirm.notFoundError(
-        sender,
-        '쪽지를 작성한 유저를 찾을 수 없습니다.',
-      );
 
       const receiver = await this.userRepository.findOne(receiverNo, {
-        select: ['no'],
         relations: ['receivedLetters', 'mailboxUsers'],
       });
-      this.errorConfirm.notFoundError(
-        receiver,
-        '쪽지를 전달받을 유저를 찾을 수 없습니다.',
-      );
-      if (sender.no === receiver.no) {
-        throw new UnauthorizedException(
-          '본인에게는 쪽지를 전송할 수 없습니다.',
-        );
-      }
 
-      const senderMailboxUserNo =
-        await this.mailboxUserRepository.saveMailboxUser(
-          mailboxRelation,
-          sender,
-        );
-      const receiverMailboxUserNo =
-        await this.mailboxUserRepository.saveMailboxUser(
-          mailboxRelation,
-          receiver,
-        );
+      console.log(sender);
+      console.log(receiver);
+
+      const mailbox = await this.mailboxRepository.findOne(mailboxNo, {
+        relations: ['letters', 'mailboxUsers'],
+      });
+      console.log(mailbox);
 
       const { insertId } = await this.letterRepository.sendLetter(
         sender,
         receiver,
+        mailbox,
         description,
-        mailboxRelation,
       );
-      console.log('mailbox ###', mailboxRelation);
-      console.log('sender ###', sender);
-      console.log('receiver ###', receiver);
-      console.log('insertid ###', insertId);
+
+      const senderMailboxUserNo =
+        await this.mailboxUserRepository.saveMailboxUser(mailbox, sender);
+      const receiverMailboxUserNo =
+        await this.mailboxUserRepository.saveMailboxUser(mailbox, receiver);
+
+      sender.sendLetters.push(insertId);
+      sender.mailboxUsers.push(senderMailboxUserNo);
+      receiver.receivedLetters.push(insertId);
+      receiver.mailboxUsers.push(receiverMailboxUserNo);
+
+      mailbox.letters.push(insertId);
+      mailbox.mailboxUsers.push(senderMailboxUserNo, receiverMailboxUserNo);
+      // const newMailboxNo = !mailboxNo
+      //   ? await this.mailboxRepository.createMailbox()
+      //   : mailboxNo;
+      // if (!newMailboxNo) {
+      //   throw new InternalServerErrorException('쪽지 보내기 에러');
+      // }
+      // const mailboxRelation = await this.mailboxRepository.findOne(
+      //   newMailboxNo,
+      //   {
+      //     select: ['no'],
+      //     relations: ['letters', 'mailboxUsers'],
+      //   },
+      // );
+      // // console.log(mailboxRelation, newMailboxNo);
+      // const sender = await this.userRepository.findOne(senderNo, {
+      //   select: ['no'],
+      //   relations: ['sendLetters', 'mailboxUsers'],
+      // });
+      // this.errorConfirm.notFoundError(
+      //   sender,
+      //   '쪽지를 작성한 유저를 찾을 수 없습니다.',
+      // );
+
+      // const receiver = await this.userRepository.findOne(receiverNo, {
+      //   select: ['no'],
+      //   relations: ['receivedLetters', 'mailboxUsers'],
+      // });
+      // this.errorConfirm.notFoundError(
+      //   receiver,
+      //   '쪽지를 전달받을 유저를 찾을 수 없습니다.',
+      // );
+      // if (sender.no === receiver.no) {
+      //   throw new UnauthorizedException(
+      //     '본인에게는 쪽지를 전송할 수 없습니다.',
+      //   );
+      // }
+
+      // const senderMailboxUserNo =
+      //   await this.mailboxUserRepository.saveMailboxUser(
+      //     mailboxRelation,
+      //     sender,
+      //   );
+      // const receiverMailboxUserNo =
+      //   await this.mailboxUserRepository.saveMailboxUser(
+      //     mailboxRelation,
+      //     receiver,
+      //   );
+
+      // const { insertId } = await this.letterRepository.sendLetter(
+      //   sender,
+      //   receiver,
+      //   description,
+      //   mailboxRelation,
+      // );
+
       // sender.sendLetters.push(insertId);
       // sender.mailboxUsers.push(senderMailboxUserNo);
 
