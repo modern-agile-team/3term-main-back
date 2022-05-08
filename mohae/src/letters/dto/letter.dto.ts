0488863bdf2e2cd893a4c130273f6c95bed580f1
Yonghoon <yonghoonj98@gmail.com>
2022-05-08 17:42:56 +0900

    Add(jungyonghoon/letters): Swagger API 태그 및 API 문서 설명 추가 #6

--- mohae/src/letters/dto/letter.dto.ts
+++ mohae/src/letters/dto/letter.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsNumber,
@@ -7,18 +8,38 @@ import {
 } from 'class-validator';
 
 export class SendLetterDto {
+  @ApiProperty({
+    example: 1,
+    description: '쪽지 보낸 유저 번호',
+    required: true,
+  })
   @IsNotEmpty()
   @IsNumber()
   senderNo: number;
 
+  @ApiProperty({
+    example: 2,
+    description: '쪽지 받은 유저 번호',
+    required: true,
+  })
   @IsNotEmpty()
   @IsNumber()
   receiverNo: number;
 
+  @ApiProperty({
+    example: null,
+    description: '쪽지함이 존재하지 않을 경우 null, 있으면 mailboxNo',
+    required: true,
+  })
   @IsOptional()
   @IsNumber()
   mailboxNo?: number;
 
+  @ApiProperty({
+    example: '쪽지 내용 전송',
+    description: '쪽지 내용',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   @MaxLength(500)
