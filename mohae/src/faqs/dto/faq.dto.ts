d6b3a0f0ee77042d2414df2fa63c1c74564f2c78
Yonghoon <yonghoonj98@gmail.com>
2022-05-08 17:58:25 +0900

    Add(jungyonghoon/faqs): Swagger API 태그 및 API 문서 내용 추가 #15

--- mohae/src/faqs/dto/faq.dto.ts
+++ mohae/src/faqs/dto/faq.dto.ts
@@ -1,23 +1,62 @@
-import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
 
 export class CreateFaqDto {
-  @IsString()
-  title: string;
-
+  @ApiProperty({
+    example: 1,
+    description: '생성한 관리자 번호',
+    required: true,
+  })
   @IsNumber()
+  @IsNotEmpty()
   managerNo: number;
 
+  @ApiProperty({
+    example: 'FAQ 제목',
+    description: 'FAQ 제목',
+    required: true,
+  })
   @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    example: 'FAQ 내용',
+    description: 'FAQ 내용',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
   description: string;
 }
 
 export class UpdateFaqDto {
-  @IsString()
-  title: string;
-
+  @ApiProperty({
+    example: 2,
+    description: '수정한 관리자 번호',
+    required: true,
+  })
   @IsNumber()
+  @IsNotEmpty()
   modifiedManagerNo: number;
 
+  @ApiProperty({
+    example: '수정한 FAQ 제목',
+    description: '수정한 FAQ 제목',
+    required: true,
+  })
   @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    example: '수정한 FAQ 내용',
+    description: '수정한 FAQ 내용',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
   description: string;
 }
