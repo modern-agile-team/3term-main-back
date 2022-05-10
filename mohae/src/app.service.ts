52713740a9b14f8687fde8f214f86e9fc7636c24
Yonghoon <yonghoonj98@gmail.com>
2022-05-10 11:51:03 +0900

    Jenkins(jungyonghoon): 젠킨스 마지막 테스트

--- mohae/src/app.service.ts
+++ mohae/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, Modern-Agile Server.';
+    return 'Hello, Modern-Agile Server!';
   }
 }
