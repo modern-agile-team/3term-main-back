042c0462f6cc102a590e00ab0ccb1831731cc39e
Yonghoon <yonghoonj98@gmail.com>
2022-05-10 15:00:13 +0900

    Jenkins(jungyonghoon): 서버 재시작 문구 수정

--- mohae/src/app.service.ts
+++ mohae/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, Modern-Agile 3term Server! Very gooooooooooood';
+    return 'Hello, Modern-Agile 3term Server! 승범';
   }
 }
