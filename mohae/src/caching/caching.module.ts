0428cce51f1b9a408fb9388afa0920b880048e7d
Yonghoon <yonghoonj98@gmail.com>
2022-05-11 10:53:48 +0900

    Delete(jungyonghoon/caching): Caching -> redis-cache 이름 변경을 위해 삭제 #182

--- mohae/src/caching/caching.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CachingController } from './caching.controller';
-import { CachingService } from './caching.service';
-
-@Module({
-  controllers: [CachingController],
-  providers: [CachingService]
-})
-export class CachingModule {}
