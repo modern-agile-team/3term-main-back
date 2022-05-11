6fa2fb04dfbc30aa4e1ac84fa2d75f4d439557eb
Yonghoon <yonghoonj98@gmail.com>
2022-05-11 11:45:36 +0900

    Add(jungyonghoon/cache): appController에 cache interceptors 적용 #182

--- mohae/src/app.controller.ts
+++ mohae/src/app.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  CacheInterceptor,
+  Controller,
+  Get,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
+// @UseInterceptors(CacheInterceptor)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- mohae/src/app.service.ts
+++ mohae/src/app.service.ts
@@ -6,7 +6,9 @@ export class AppService {
   constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
 
   async getHello() {
-    await this.cacheManager.set('cached_item', { key: 32 });
+    await this.cacheManager.set('cached_item', { key: 32 }, { ttl: 10 });
+    await this.cacheManager.del('cached_item');
+    await this.cacheManager.reset();
     const cachedItem = await this.cacheManager.get('cached_item');
     console.log(cachedItem);
 
