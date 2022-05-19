0a2e547dcdf17712c1f28f24673019c9623565b0
Yonghoon <yonghoonj98@gmail.com>
2022-05-19 21:57:37 +0900

    Delete(jungyonghoon/file): 파일 저장 테스트 로직 삭제

--- mohae/src/app.controller.ts
+++ mohae/src/app.controller.ts
@@ -3,14 +3,8 @@ import {
   CacheKey,
   CacheTTL,
   Controller,
-  Delete,
   Get,
-  Post,
-  UploadedFile,
-  UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { AwsService } from './aws/aws.service';
 
@@ -28,11 +22,4 @@ export class AppController {
   // async getHello() {
   //   return await this.appService.getHello();
   // }
-
-  @ApiOperation({ summary: '이미지 업로드' })
-  @UseInterceptors(FileInterceptor('image'))
-  @Post('/upload')
-  async uploadImg(@UploadedFile() file: Express.Multer.File) {
-    return await this.awsService.uploadFileToS3('profiles', file);
-  }
 }
