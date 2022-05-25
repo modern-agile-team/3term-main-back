6e4ab1ee65b86cb7d3f615d9a2d2ee20a30c68df
Yonghoon <yonghoonj98@gmail.com>
2022-05-25 14:09:01 +0900

    Fix(jungyonghoon/sentry): Sentry 연동 에러 이슈 해결 #7

--- mohae/src/reviews/dto/create-review.dto.ts
+++ mohae/src/reviews/dto/create-review.dto.ts
@@ -1,12 +1,24 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Review } from '../entity/review.entity';
-
-export class CreateReviewDto extends PickType(Review, [
-  'description',
-  'rating',
-] as const) {
+// extends PickType(Review, [
+//   'description',
+//   'rating',
+// ] as const)
+export class CreateReviewDto {
   @IsNotEmpty()
   @IsNumber()
-  board: any;
+  board: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  rating: number;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  reviewer: number;
 }
--- mohae/src/reviews/entity/review.entity.ts
+++ mohae/src/reviews/entity/review.entity.ts
@@ -23,18 +23,18 @@ export class Review extends BaseEntity {
   @PrimaryGeneratedColumn()
   no: number;
 
-  @IsNotEmpty({ message: '내용을 작성해 주세요.' })
-  @IsString()
-  @MaxLength(100, { message: '100자 이내로 입력해 주세요.' })
+  // @IsNotEmpty({ message: '내용을 작성해 주세요.' })
+  // @IsString()
+  // @MaxLength(100, { message: '100자 이내로 입력해 주세요.' })
   @Column({
     type: 'mediumtext',
     comment: '리뷰 작성할 때 내용이 들어감',
   })
   description: string;
 
-  @IsNotEmpty({ message: '점수를 입력해 주세요.' })
-  @IsNumber()
-  @Length(1, 5, { message: '1점 ~ 5점 사이 평점을 남겨주세요.' })
+  // @IsNotEmpty({ message: '점수를 입력해 주세요.' })
+  // @IsNumber()
+  // @Length(1, 5, { message: '1점 ~ 5점 사이 평점을 남겨주세요.' })
   @Column({
     type: 'int',
     comment: '리뷰 작성할 때 평점이 들어감',
@@ -63,6 +63,8 @@ export class Review extends BaseEntity {
   })
   board: Board;
 
-  @ManyToOne((type) => User, (user) => user.reviews)
+  @ManyToOne((type) => User, (user) => user.reviews, {
+    onDelete: 'SET NULL',
+  })
   reviewer: User;
 }
