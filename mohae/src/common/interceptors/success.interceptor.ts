d6d607aa8144e9b098ac05d7f7e27a4ac6751ff9
Yonghoon <yonghoonj98@gmail.com>
2022-05-27 17:53:14 +0900

    Refactor(jungyonghoon/communication): 인터셉터 적용

--- mohae/src/common/interceptors/success.interceptor.ts
+++ mohae/src/common/interceptors/success.interceptor.ts
@@ -10,16 +10,14 @@ import { map, Observable } from 'rxjs';
 @Injectable()
 export class SuccesseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const statusCode: Response = context
-      .switchToHttp()
-      .getResponse().statusCode;
+    const statusCode: number = context.switchToHttp().getResponse().statusCode;
 
     return next.handle().pipe(
       map((data) => ({
         success: true,
+        statusCode: data.statusCode | statusCode,
         msg: data.msg,
         response: data.response,
-        statusCode,
       })),
     );
   }
