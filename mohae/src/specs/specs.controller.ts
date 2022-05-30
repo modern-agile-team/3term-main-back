9b8ddcd339d8b5d15f44abc9c3e87c62cf6bc6cd
subro <clark9810@naver.com>
2022-05-30 11:49:25 +0900

    Add(leesuhyung/spec): 스펙 컨트롤러에  SuccessInterceptor 적용 #99

--- mohae/src/specs/specs.controller.ts
+++ mohae/src/specs/specs.controller.ts
@@ -11,6 +11,7 @@ import {
   UseInterceptors,
   Query,
   Put,
+  HttpCode,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FilesInterceptor } from '@nestjs/platform-express';
@@ -24,11 +25,13 @@ import {
 import { User } from '@sentry/node';
 import { AwsService } from 'src/aws/aws.service';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { SuccesseInterceptor } from 'src/common/interceptors/success.interceptor';
 import { CreateSpecDto, UpdateSpecDto } from './dto/spec.dto';
 import { Spec } from './entity/spec.entity';
 import { SpecsService } from './specs.service';
 
 @Controller('specs')
+@UseInterceptors(SuccesseInterceptor)
 @ApiTags('스펙 API')
 export class SpecsController {
   constructor(
@@ -38,6 +41,7 @@ export class SpecsController {
   ) {}
 
   @Get('user/:profileUserNo')
+  @HttpCode(200)
   @ApiOperation({
     summary: '한 명의 유저 스펙 전체 조회 API',
     description: '한명의 유저 스펙을 한번에 불러온다',
@@ -48,21 +52,23 @@ export class SpecsController {
   @ApiInternalServerErrorResponse({
     description: '스펙 전체 조회 관련 서버 에러.',
   })
-  async getAllSpec(@Param('profileUserNo') profileUserNo: number) {
+  async getAllSpec(
+    @Param('profileUserNo') profileUserNo: number,
+  ): Promise<object> {
     try {
-      const specs: Spec = await this.specsService.getAllSpec(profileUserNo);
+      const response: Spec = await this.specsService.getAllSpec(profileUserNo);
 
-      return Object.assign({
-        statusCode: 200,
+      return {
         msg: '성공적으로 스펙을 불러왔습니다.',
-        specs,
-      });
+        response,
+      };
     } catch (err) {
       throw err;
     }
   }
 
   @Get('spec/:specNo')
+  @HttpCode(200)
   @ApiOperation({
     summary: '스펙 상세조회 API',
     description: '하나의 스펙을 조회 해준다.',
@@ -71,37 +77,36 @@ export class SpecsController {
     description: '성공적으로 스펙이 불러와진 경우.',
   })
   @ApiResponse({ status: 404, description: '해당 스펙이 존재하지 않은 경우' })
-  async getOneSpec(@Param('specNo') specNo: number) {
+  async getOneSpec(@Param('specNo') specNo: number): Promise<object> {
     try {
-      const spec: Spec = await this.specsService.getOneSpec(specNo);
+      const response: Spec = await this.specsService.getOneSpec(specNo);
 
-      return Object.assign({
-        statusCode: 200,
+      return {
         msg: '성공적으로 스펙을 불러왔습니다.',
-        spec,
-      });
+        response,
+      };
     } catch (err) {
       throw err;
     }
   }
 
   @Get('profile')
+  @HttpCode(200)
   async readUserSpec(
     @Query('user') user: number,
     @Query('take') take: number,
     @Query('page') page: number,
-  ) {
+  ): Promise<object> {
     try {
-      const response: Array<Spec> = await this.specsService.readUserSpec(
+      const response: Spec[] = await this.specsService.readUserSpec(
         user,
         take,
         page,
       );
-      return Object.assign({
-        statusCode: 200,
+      return {
         msg: '프로필 스펙 조회에 성공했습니다.',
         response,
-      });
+      };
     } catch (err) {
       throw err;
     }
@@ -109,12 +114,13 @@ export class SpecsController {
 
   @UseInterceptors(FilesInterceptor('image', 10))
   @Post('regist')
+  @HttpCode(201)
   @UseGuards(AuthGuard())
   async registSpec(
     @UploadedFiles() files: Express.Multer.File[],
     @Body() createSpecDto: CreateSpecDto,
     @CurrentUser() user: User,
-  ) {
+  ): Promise<object> {
     try {
       const specPhotoUrls =
         files[0].originalname === 'default.jpg'
@@ -123,10 +129,9 @@ export class SpecsController {
 
       await this.specsService.registSpec(user.no, specPhotoUrls, createSpecDto);
 
-      return Object.assign({
-        statusCode: 201,
+      return {
         msg: '성공적으로 스팩등록이 되었습니다.',
-      });
+      };
     } catch (err) {
       throw err;
     }
@@ -134,11 +139,12 @@ export class SpecsController {
 
   @UseInterceptors(FilesInterceptor('image', 10))
   @Put(':no')
+  @HttpCode(204)
   async updateSpec(
     @Param('no') specNo: number,
     @UploadedFiles() files: Express.Multer.File[],
     @Body() updateSpecdto: UpdateSpecDto,
-  ) {
+  ): Promise<object> {
     try {
       const specPhotoUrls =
         files.length === 0
@@ -153,24 +159,23 @@ export class SpecsController {
 
       await this.awsService.deleteSpecS3Object(originSpecPhotoUrls);
 
-      return Object.assign({
-        statusCode: 204,
+      return {
         msg: '성공적으로 스팩이 수정되었습니다.',
-      });
+      };
     } catch (err) {
       throw err;
     }
   }
 
   @Delete(':no')
-  async deleteSpec(@Param('no') specNo: number) {
+  @HttpCode(204)
+  async deleteSpec(@Param('no') specNo: number): Promise<object> {
     try {
       await this.specsService.deleteSpec(specNo);
 
-      return Object.assign({
-        statusCode: 204,
+      return {
         msg: '성공적으로 스팩을 삭제하였습니다.',
-      });
+      };
     } catch (err) {
       throw err;
     }
