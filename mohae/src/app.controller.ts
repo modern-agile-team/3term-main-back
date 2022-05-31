4500e4df8556e103a99e3cd252d9de19cce4d7ca
Yonghoon <yonghoonj98@gmail.com>
2022-05-31 15:32:33 +0900

    Add(imageUpload): 이미지 업로드 로직에 필요한 라이브러리 및 multer 옵션, 코드 추가

--- mohae/src/app.controller.ts
+++ mohae/src/app.controller.ts
@@ -1,12 +1,13 @@
 import {
-  CacheInterceptor,
-  CacheKey,
-  CacheTTL,
   Controller,
-  Get,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 import { AwsService } from './aws/aws.service';
+import { multerOptions } from './common/configs/multer.option';
 
 // @UseInterceptors(CacheInterceptor)
 @Controller()
@@ -22,4 +23,12 @@ export class AppController {
   // async getHello() {
   //   return await this.appService.getHello();
   // }
+
+  @Post('image')
+  @UseInterceptors(FilesInterceptor('image', 10, multerOptions))
+  async imageResizing(@UploadedFiles() files: Express.Multer.File) {
+    const savedImage = await this.awsService.uploadFileToS3('resize', files);
+
+    return savedImage;
+  }
 }
--- mohae/src/aws/aws.service.ts
+++ mohae/src/aws/aws.service.ts
@@ -1,10 +1,8 @@
 import * as path from 'path';
 import * as AWS from 'aws-sdk';
+import * as sharp from 'sharp';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PromiseResult } from 'aws-sdk/lib/request';
-
-// sharp 패키지로 이미지 변환 가능
 
 @Injectable()
 export class AwsService {
@@ -17,33 +15,48 @@ export class AwsService {
       secretAccessKey: this.configService.get('AWS_S3_SECRET_KEY'),
       region: this.configService.get('AWS_S3_REGION'),
     });
-    this.S3_BUCKET_NAME = this.configService.get('AWS_S3_BUCKET_NAME'); // nestcatstudy
+    this.S3_BUCKET_NAME = this.configService.get('AWS_S3_BUCKET_NAME');
   }
 
   async uploadFileToS3(
     folder: string,
-    file: Express.Multer.File,
+    files: Express.Multer.File,
   ): Promise<{
-    key: string;
-    s3Object: PromiseResult<AWS.S3.PutObjectOutput, AWS.AWSError>;
+    keys: Array<string>;
+    // s3Object: PromiseResult<AWS.S3.PutObjectOutput, AWS.AWSError>;
     contentType: string;
   }> {
     try {
-      const key = `${folder}/${Date.now()}_${path.basename(
-        file.originalname,
-      )}`.replace(/ /g, '');
+      const keys = [];
 
-      const s3Object = await this.awsS3
-        .putObject({
-          Bucket: this.S3_BUCKET_NAME,
-          Key: key,
-          Body: file.buffer,
-          ACL: 'public-read',
-          ContentType: file.mimetype,
-        })
-        .promise();
+      for (const index in files) {
+        const key: string = `${folder}/${Date.now()}_${path.basename(
+          files[index].originalname,
+        )}`.replace(/ /g, '');
 
-      return { key, s3Object, contentType: file.mimetype };
+        sharp(files[index].buffer)
+          .resize({ width: 100 }) // 비율을 유지하며 가로 크기 줄이기
+          .withMetadata() // 이미지의 exif데이터 유지
+          .toBuffer((err, buffer) => {
+            if (err) {
+              throw err;
+            }
+
+            this.awsS3
+              .putObject({
+                Bucket: this.S3_BUCKET_NAME,
+                Key: key,
+                Body: buffer,
+                ACL: 'public-read',
+                ContentType: files[index].mimetype,
+              })
+              .promise();
+          });
+
+        keys.push(key);
+      }
+
+      return { keys, contentType: files.mimetype };
     } catch (error) {
       throw new BadRequestException(`File upload failed : ${error}`);
     }
@@ -72,4 +85,19 @@ export class AwsService {
   public getAwsS3FileUrl(objectKey: string) {
     return `https://${this.S3_BUCKET_NAME}.s3.amazonaws.com/${objectKey}`;
   }
+
+  // upload = multer({
+  //   storage: s3Storage({
+  //     s3: awsS3,
+  //     bucket: this.S3,
+  //     acl: 'public-read',
+  //     key: function (request, file, cb) {
+  //       cb(null, `${Date.now().toString()} - ${file.originalname}`);
+  //     },
+  //     resize: {
+  //       width: 600,
+  //       height: 400,
+  //     },
+  //   }),
+  // }).array('upload', 1);
 }
