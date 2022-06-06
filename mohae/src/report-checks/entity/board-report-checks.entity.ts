f284809a4cf4cf022695952d7d51cd64b67af286
Yonghoon <yonghoonj98@gmail.com>
2022-06-06 16:23:17 +0900

    Refactor(jungyonghoon/report): 신고-체크내용 테이블 관계 리팩토링

--- mohae/src/report-checks/entity/board-report-checks.entity.ts
+++ mohae/src/report-checks/entity/board-report-checks.entity.ts
@@ -1,17 +1,35 @@
 import { ReportCheckbox } from 'src/report-checkboxes/entity/report-checkboxes.entity';
 import { ReportedBoard } from 'src/reports/entity/reported-board.entity';
-import { BaseEntity, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('board_report_checks')
 export class BoardReportChecks extends BaseEntity {
   @PrimaryGeneratedColumn()
   no: number;
 
-  @ManyToOne((type) => ReportCheckbox, (checks) => checks.reportedBoards, {
-    nullable: true,
+  @ManyToOne(() => ReportCheckbox, (checks) => checks.reportedBoards, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({
+    name: 'check_no',
   })
   check: ReportCheckbox;
 
-  @ManyToOne((type) => ReportedBoard, (report) => report.checks)
+  @ManyToOne(() => ReportedBoard, (reportedBoards) => reportedBoards.checks, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({
+    name: 'reported_board_no',
+  })
   reportedBoard: ReportedBoard;
 }
--- mohae/src/report-checks/entity/user-report-checks.entity.ts
+++ mohae/src/report-checks/entity/user-report-checks.entity.ts
@@ -1,5 +1,11 @@
 import { ReportedUser } from 'src/reports/entity/reported-user.entity';
-import { BaseEntity, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { ReportCheckbox } from '../../report-checkboxes/entity/report-checkboxes.entity';
 
 @Entity('user_report_checks')
@@ -7,11 +13,23 @@ export class UserReportChecks extends BaseEntity {
   @PrimaryGeneratedColumn()
   no: number;
 
-  @ManyToOne((type) => ReportCheckbox, (checks) => checks.reportedUsers, {
-    nullable: true,
+  @ManyToOne(() => ReportCheckbox, (checks) => checks.reportedUsers, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({
+    name: 'check_no',
   })
   check: ReportCheckbox;
 
-  @ManyToOne((type) => ReportedUser, (report) => report.checks)
+  @ManyToOne(() => ReportedUser, (reportedUsers) => reportedUsers.checks, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({
+    name: 'reported_user_no',
+  })
   reportedUser: ReportedUser;
 }
