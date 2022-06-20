6383d0fa54488dba8c5451cdf0a6f74bfc21b31f
Yonghoon <yonghoonj98@gmail.com>
2022-06-20 16:11:49 +0900

    Fix(jungyonghoon/comment): 조회 이슈 해결 #277

--- mohae/src/common/utils/error.ts
+++ mohae/src/common/utils/error.ts
@@ -1,14 +1,21 @@
 import {
   BadGatewayException,
   BadRequestException,
+  Inject,
   InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
+import { WinstonLogger, WINSTON_MODULE_PROVIDER } from 'nest-winston';
 // isNotExist 방식으로
 export class ErrorConfirm {
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: WinstonLogger,
+  ) {}
+
   notFoundError(param: any, msg: string | undefined) {
     if (!param) {
+      this.logger.error('Not found exception error => ', msg);
       throw new NotFoundException(`${msg}`);
     }
   }
