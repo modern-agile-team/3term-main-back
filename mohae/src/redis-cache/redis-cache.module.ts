913dc054f72cd6293fee06d196e50585aff7c1d7
Yonghoon <yonghoonj98@gmail.com>
2022-06-24 11:59:07 +0900

    Modify(jungyonghoon/redis): FAQ에 REDIS 적용

--- mohae/src/redis-cache/redis-cache.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedisCacheController } from './redis-cache.controller';
-import { RedisCacheService } from './redis-cache.service';
-
-@Module({
-  controllers: [RedisCacheController],
-  providers: [RedisCacheService],
-})
-export class RedisCacheModule {}
