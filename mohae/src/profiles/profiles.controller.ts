0bdbfcbd0376eeb1810233c0d9868e670ddfd4b7
subro <clark9810@naver.com>
2022-07-16 16:06:54 +0900

    Refactor(leesuhyung/swagger): profile controller swagger관련 코드 최적화

--- mohae/src/profiles/profiles.controller.ts
+++ mohae/src/profiles/profiles.controller.ts
@@ -26,9 +26,11 @@ import { User } from '@sentry/node';
 import { AwsService } from 'src/aws/aws.service';
 import { HTTP_STATUS_CODE } from 'src/common/configs/http-status.config';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { operationConfig } from 'src/common/swagger-apis/api-operation.swagger';
 import { JudgeDuplicateNicknameDto } from './dto/judge-duplicate-nickname.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { ProfilesService } from './profiles.service';
+import { profileSwagger } from './profiles.swagger';
 
 @Controller('profile')
 @ApiTags('Profile')
@@ -38,49 +40,16 @@ export class ProfilesController {
     private readonly awsService: AwsService,
   ) {}
 
-  @ApiOperation({
-    summary: '한 명의 유저 프로필 전체 조회 API',
-    description: '한명의 유저 프로필을 한번에 불러온다',
-  })
-  @ApiOkResponse({
-    description: '성공적으로 유저의 프로필이 불러와진 경우.',
-    schema: {
-      example: {
-        statusCode: 200,
-        msg: '프로필 조회에 성공했습니다.',
-      },
-    },
-  })
-  @ApiUnauthorizedResponse({
-    description: '회원가입하지 않은 사람이 프로필 조회를 시도한 경우',
-    schema: {
-      example: {
-        statusCode: 401,
-        msg: 'Unauthorized',
-        err: 'Unauthorized',
-      },
-    },
-  })
-  @ApiNotFoundResponse({
-    description: '조회 하려는 회원이 존재하지 않는 경우',
-    schema: {
-      example: {
-        statusCode: 404,
-        msg: '~에 해당하는 회원을 찾을 수 없습니다.',
-        err: 'Not Found',
-      },
-    },
-  })
-  @ApiInternalServerErrorResponse({
-    description: '요청에 대한 응답 처리중 서버에러가 발생한 경우',
-    schema: {
-      example: {
-        statusCode: 500,
-        msg: 'DB관련한 에러 메시지 + ~에서 일어난 에러입니다.',
-        err: 'InternalServerErrorException',
-      },
-    },
-  })
+  @ApiOperation(
+    operationConfig(
+      '한 명의 유저 프로필 전체 조회 API',
+      '한명의 유저 프로필을 한번에 불러온다',
+    ),
+  )
+  @ApiOkResponse(profileSwagger.readUserProfile.success)
+  @ApiUnauthorizedResponse(profileSwagger.readUserProfile.unauthorizedResponse)
+  @ApiNotFoundResponse(profileSwagger.readUserProfile.notFoundResponse)
+  @ApiInternalServerErrorResponse(profileSwagger.internalServerErrorResponse)
   @UseGuards(AuthGuard('jwt'))
   @ApiBearerAuth('access-token')
   @HttpCode(HTTP_STATUS_CODE.success.ok)
@@ -99,41 +68,15 @@ export class ProfilesController {
     };
   }
 
-  @ApiOperation({
-    summary: '프로필 수정에서 닉네임 변경확인 API',
-    description: '변경할 수 있는 닉네임인지 확인한다.',
-  })
-  @ApiOkResponse({
-    description: '사용가능한 닉네임인 경우.',
-    schema: {
-      example: {
-        success: true,
-        statusCode: 200,
-        msg: '사용가능한 닉네임입니다.',
-      },
-    },
-  })
-  @ApiConflictResponse({
-    description:
-      '변경하려는 닉네임이 현재 닉네임이거나 다른사람이 사용하는 경우.',
-    schema: {
-      example: {
-        statusCode: 409,
-        msg: '현재 닉네임입니다. or 이미 사용 중인 닉네임 입니다.',
-        err: 'Conflict',
-      },
-    },
-  })
-  @ApiInternalServerErrorResponse({
-    description: '요청에 대한 응답 처리중 서버에러가 발생한 경우',
-    schema: {
-      example: {
-        statusCode: 500,
-        msg: 'DB관련한 에러 메시지 + ~에서 일어난 에러입니다.',
-        err: 'InternalServerErrorException',
-      },
-    },
-  })
+  @ApiOperation(
+    operationConfig(
+      '프로필 수정에서 닉네임 변경확인 API',
+      '변경할 수 있는 닉네임인지 확인한다.',
+    ),
+  )
+  @ApiOkResponse(profileSwagger.judgeDuplicateNickname.success)
+  @ApiConflictResponse(profileSwagger.judgeDuplicateNickname.confilctResponse)
+  @ApiInternalServerErrorResponse(profileSwagger.internalServerErrorResponse)
   @HttpCode(HTTP_STATUS_CODE.success.ok)
   @ApiBearerAuth('access-token')
   @Post('/check-nickname')
@@ -147,50 +90,15 @@ export class ProfilesController {
     };
   }
 
-  @ApiOperation({
-    summary: '유저 프로필 정보 수정 API',
-    description: '유저 프로필 정보를 수정한다.',
-  })
-  @ApiOkResponse({
-    description: '성공적으로 유저의 프로필이 수정된 경우.',
-    schema: {
-      example: {
-        success: true,
-        statusCode: 201,
-        msg: '프로필 정보 수정이 완료되었습니다.',
-      },
-    },
-  })
-  @ApiUnauthorizedResponse({
-    description: '회원가입하지 않은 사람이 프로필 조회를 시도한 경우',
-    schema: {
-      example: {
-        statusCode: 401,
-        msg: 'Unauthorized',
-        err: 'Unauthorized',
-      },
-    },
-  })
-  @ApiNotFoundResponse({
-    description: '변경하려는 학교(전공)이 DB에 존재하지 않는 학교(전공)인 경우',
-    schema: {
-      example: {
-        statusCode: 404,
-        msg: '~에 해당하는 학교(전공)을 찾을 수 없습니다.',
-        err: 'Not Found',
-      },
-    },
-  })
-  @ApiInternalServerErrorResponse({
-    description: '요청에 대한 응답 처리중 서버에러가 발생한 경우',
-    schema: {
-      example: {
-        statusCode: 500,
-        msg: 'DB관련한 에러 메시지 + ~에서 일어난 에러입니다.',
-        err: 'InternalServerErrorException',
-      },
-    },
-  })
+  @ApiOperation(
+    operationConfig(
+      '유저 프로필 정보 수정 API',
+      '유저 프로필 정보를 수정한다.',
+    ),
+  )
+  @ApiOkResponse(profileSwagger.updateProfile.success)
+  @ApiUnauthorizedResponse(profileSwagger.updateProfile.unauthorizedResponse)
+  @ApiInternalServerErrorResponse(profileSwagger.internalServerErrorResponse)
   @HttpCode(HTTP_STATUS_CODE.success.ok)
   @ApiBearerAuth('access-token')
   @UseGuards(AuthGuard('jwt'))
