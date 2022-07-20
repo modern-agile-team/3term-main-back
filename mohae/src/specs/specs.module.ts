7ed3df3bacce071225383427a20f6e39c27e121e
subro <clark9810@naver.com>
2022-07-20 16:13:20 +0900

    Refactor(leesuhyung/spec): spec 모듈 import 부분 깔끔하게 정리

--- mohae/src/specs/specs.module.ts
+++ mohae/src/specs/specs.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
-import { UserRepository } from 'src/auth/repository/user.repository';
 import { PhotoModule } from 'src/photo/photo.module';
-import { SpecPhotoRepository } from 'src/photo/repository/photo.repository';
-import { ErrorConfirm } from 'src/common/utils/error';
-import { SpecRepository } from './repository/spec.repository';
 import { SpecsController } from './specs.controller';
 import { SpecsService } from './specs.service';
 import { AwsService } from 'src/aws/aws.service';
+import { SpecRepository } from './repository/spec.repository';
+import { SpecPhotoRepository } from 'src/photo/repository/photo.repository';
+import { UserRepository } from 'src/auth/repository/user.repository';
+import { ErrorConfirm } from 'src/common/utils/error';
 
 @Module({
   imports: [
